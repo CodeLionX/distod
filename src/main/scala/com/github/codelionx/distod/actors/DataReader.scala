ea369f8cf1b1265614d528ee52ea523d3a76f00b
CodeLionX <sebastian.schmidl@t-online.de>
2019-11-15 14:20:27 +0100

    read CSV in own actor and parallelize partitioning step

--- src/main/scala/com/github/codelionx/distod/actors/DataReader.scala
+++ src/main/scala/com/github/codelionx/distod/actors/DataReader.scala
@@ -1,24 +1,71 @@
 package com.github.codelionx.distod.actors
 
 import akka.NotUsed
-import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
+import com.github.codelionx.distod.Serialization.CborSerializable
 import com.github.codelionx.distod.Settings
+import com.github.codelionx.distod.actors.LeaderGuardian.DataLoaded
 import com.github.codelionx.distod.io.CSVParser
+import com.github.codelionx.distod.partitions.{FullPartition, Partition}
 
 
 object DataReader {
 
   val name = "data-reader"
 
-  def apply(): Behavior[NotUsed] = Behaviors.setup { context =>
-    import context._
+  sealed trait Command extends CborSerializable
 
-    val settings = Settings(system)
+  private final case class PartitioningFinished(columnId: Int, partition: FullPartition) extends Command
 
-    val parser = CSVParser(settings)
-    val table = parser.parse()
-    println(table)
-    Behaviors.stopped
+  def apply(replyTo: ActorRef[DataLoaded]): Behavior[Command] = Behaviors.setup { context =>
+    new DataReader(context, replyTo).start()
   }
+
+  def partitioner(columnId: Int, column: Array[String], replyTo: ActorRef[PartitioningFinished]): Behavior[NotUsed] =
+    Behaviors.setup { context =>
+      context.log.info("Partitioning column {}", columnId)
+      val partition = Partition.fullFrom(column)
+      replyTo ! PartitioningFinished(columnId, partition)
+      Behaviors.stopped
+    }
+}
+
+
+class DataReader(
+                  context: ActorContext[DataReader.Command],
+                  replyTo: ActorRef[DataLoaded]
+                ) {
+
+  import DataReader._
+
+
+  private val settings = Settings(context.system)
+  private val parser = CSVParser(settings)
+
+  context.log.info("DataReader started, parsing data from {}", settings.inputParsingSettings.filePath)
+  private val table = parser.parse()
+
+  private def start(): Behavior[Command] = {
+    // send columns out to temp actors
+    table.columns.zipWithIndex.foreach { case (column, id) =>
+      context.spawnAnonymous(partitioner(id, column, context.self))
+    }
+
+    // collect results
+    collectPartitions(Map.empty, table.columns.length)
+  }
+
+  private def collectPartitions(partitions: Map[Int, FullPartition], expected: Int): Behavior[Command] =
+    Behaviors.receiveMessagePartial {
+      case PartitioningFinished(columnId, partition) =>
+        val newPartitions = partitions.updated(columnId, partition)
+        context.log.info("Received partition for column {}, ({}/{})", columnId, newPartitions.size, expected)
+        if (newPartitions.size == expected) {
+          replyTo ! DataLoaded(table.name, table.headers, partitions.toSeq.sortBy(_._1).map(_._2).toArray)
+          Behaviors.stopped
+        } else {
+          collectPartitions(newPartitions, expected)
+        }
+    }
 }

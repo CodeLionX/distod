6fd55292b5370706634ca9ddd643bac9a102920c
CodeLionX <sebastian.schmidl@t-online.de>
2019-11-18 01:34:30 +0100

    add master skeleton

---
+++ src/main/scala/com/github/codelionx/distod/actors/Master.scala
@@ -0,0 +1,21 @@
+package com.github.codelionx.distod.actors
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import com.github.codelionx.distod.Serialization.CborSerializable
+import com.github.codelionx.distod.actors.Master.Command
+
+
+object Master {
+
+  sealed trait Command extends CborSerializable
+
+  def apply(): Behavior[Command] = Behaviors.setup( context => new Master(context).start())
+}
+
+class Master(context: ActorContext[Command]) {
+
+  def start(): Behavior[Command] = Behaviors.receiveMessage{
+    case _ => ???
+  }
+}

26da01b3b3dc5993722ed757eb94b5cdf28c4a90
CodeLionX <sebastian.schmidl@t-online.de>
2019-11-20 22:22:34 +0100

    generate L0 and L1 candidates

--- src/main/scala/com/github/codelionx/distod/actors/Master.scala
+++ src/main/scala/com/github/codelionx/distod/actors/Master.scala
@@ -1,9 +1,9 @@
 package com.github.codelionx.distod.actors
 
-import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import com.github.codelionx.distod.actors.Master.{Command, LocalPeers}
-import com.github.codelionx.distod.partitions.StrippedPartition
+import com.github.codelionx.distod.partitions.{FullPartition, StrippedPartition}
 import com.github.codelionx.distod.protocols.DataLoadingProtocol._
 import com.github.codelionx.distod.protocols.PartitionManagementProtocol
 import com.github.codelionx.distod.protocols.PartitionManagementProtocol._
@@ -19,23 +19,23 @@ object Master {
   val name = "master"
 
   def apply(
-             guardian: ActorRef[LeaderGuardian.Command],
-             dataReader: ActorRef[DataLoadingCommand],
-             partitionManager: ActorRef[PartitionCommand]
-           ): Behavior[Command] = Behaviors.setup(context =>
+      guardian: ActorRef[LeaderGuardian.Command],
+      dataReader: ActorRef[DataLoadingCommand],
+      partitionManager: ActorRef[PartitionCommand]
+  ): Behavior[Command] = Behaviors.setup(context =>
     new Master(context, LocalPeers(guardian, dataReader, partitionManager)).start()
   )
 
   case class LocalPeers(
-                         guardian: ActorRef[LeaderGuardian.Command],
-                         dataReader: ActorRef[DataLoadingCommand],
-                         partitionManager: ActorRef[PartitionCommand]
-                       )
+      guardian: ActorRef[LeaderGuardian.Command],
+      dataReader: ActorRef[DataLoadingCommand],
+      partitionManager: ActorRef[PartitionCommand]
+  )
   case class CandidateState(
-                             splitCandidates: Seq[Int],
-                             swapCandidates: Seq[(Int, Int)],
-                             isValid: Boolean
-                           )
+      splitCandidates: Seq[Int],
+      swapCandidates: Seq[(Int, Int)],
+      isValid: Boolean
+  )
 }
 
 class Master(context: ActorContext[Command], localPeers: LocalPeers) {
@@ -48,41 +48,38 @@ class Master(context: ActorContext[Command], localPeers: LocalPeers) {
 
   private def initialize(): Behavior[Command] = Behaviors.setup { context =>
     val loadingEventMapper = context.messageAdapter(e => WrappedLoadingEvent(e))
-    val partitionEventMapper = context.messageAdapter(e => WrappedPartitionEvent(e))
 
     dataReader ! LoadPartitions(loadingEventMapper)
 
     def onLoadingEvent(loadingEvent: DataLoadingEvent): Behavior[Command] = loadingEvent match {
-      case PartitionsLoaded(t@PartitionedTable(name, headers, partitions)) =>
-        context.log.info("Finished loading dataset {}", name)
-        println(s"Table headers: ${headers.mkString(",")}")
-        println(partitions.map(p =>
-          s"Partition(numberClasses=${p.numberClasses},numberElements=${p.numberElements})"
-        ).mkString("\n"))
+      case PartitionsLoaded(t @ PartitionedTable(name, headers, partitions)) =>
+        context.log.info("Finished loading dataset {} with headers: {}", name, headers.mkString(","))
 
         // stop data reader to free up resources
         dataReader ! Stop
 
-        // generate first level candidates
-        val L1candidates = generateFirstLevelCandidates(t.m)
+        val attributes = 0 until t.nAttributes
 
-        // insert empty partition
-        partitionManager ! InsertPartition(CandidateSet.empty, StrippedPartition(
-          numberElements = t.n,
-          numberClasses = 1,
-          equivClasses = IndexedSeq((0 until t.n).toSet)
-        ))
-        // insert level1 partitions
-        L1candidates.zipWithIndex.foreach { case (candidate, index) =>
-          partitionManager ! InsertPartition(candidate, partitions(index))
+        // L0: root candidate node
+        val rootCandidateState = generateLevel0(attributes, t.nTuples)
 
-        }
+        // L1: single attribute candidate nodes
+        val L1candidateState = generateLevel1(attributes, partitions)
 
-        // ask for advanced partitions as a test
-        partitionManager ! LookupStrippedPartition(CandidateSet(0, 1, 2), partitionEventMapper)
-
-        Behaviors.same
+        testPartitionMgmt()
+//        behavior(rootCandidateState ++ L1candidateState)
     }
+
+    Behaviors.receiveMessagePartial[Command] {
+      case WrappedLoadingEvent(event) => onLoadingEvent(event)
+    }
+  }
+
+  private def testPartitionMgmt(): Behavior[Command] = {
+    val partitionEventMapper = context.messageAdapter(e => WrappedPartitionEvent(e))
+
+    // ask for advanced partitions as a test
+    partitionManager ! LookupStrippedPartition(CandidateSet(0, 1, 2), partitionEventMapper)
 
     def onPartitionEvent(event: PartitionEvent): Behavior[Command] = event match {
       case PartitionFound(key, value) =>
@@ -96,11 +93,9 @@ class Master(context: ActorContext[Command], localPeers: LocalPeers) {
         finished()
     }
 
-    Behaviors
-      .receiveMessage[Command] {
-        case WrappedLoadingEvent(event) => onLoadingEvent(event)
-        case WrappedPartitionEvent(event) => onPartitionEvent(event)
-      }
+    Behaviors.receiveMessagePartial {
+      case WrappedPartitionEvent(event) => onPartitionEvent(event)
+    }
   }
 
   private def behavior(state: Map[CandidateSet, CandidateState]): Behavior[Command] = Behaviors.receiveMessage {
@@ -112,7 +107,37 @@ class Master(context: ActorContext[Command], localPeers: LocalPeers) {
     Behaviors.stopped
   }
 
+  private def generateLevel0(attributes: Seq[Int], nTuples: Int): Map[CandidateSet, CandidateState] = {
+    val L0CandidateState = Map(
+      CandidateSet.empty -> CandidateState(
+        splitCandidates = attributes,
+        swapCandidates = Seq.empty,
+        isValid = true
+      )
+    )
+    partitionManager ! InsertPartition(CandidateSet.empty, StrippedPartition(
+      numberElements = nTuples,
+      numberClasses = 1,
+      equivClasses = IndexedSeq((0 until nTuples).toSet)
+    ))
 
-  private def generateFirstLevelCandidates(nAttributes: Int): IndexedSeq[CandidateSet] =
-    (0 until nAttributes).map(columnId => CandidateSet(columnId))
+    L0CandidateState
+  }
+
+  private def generateLevel1(
+      attributes: Seq[Int], partitions: Array[FullPartition]
+  ): Map[CandidateSet, CandidateState] = {
+    val L1candidates = attributes.map(columnId => CandidateSet(columnId))
+    val L1candidateState = L1candidates.map { candidate =>
+      candidate -> CandidateState(
+        splitCandidates = attributes,
+        swapCandidates = Seq.empty,
+        isValid = true
+      )
+    }
+    L1candidates.zipWithIndex.foreach { case (candidate, index) =>
+      partitionManager ! InsertPartition(candidate, partitions(index))
+    }
+    L1candidateState.toMap
+  }
 }

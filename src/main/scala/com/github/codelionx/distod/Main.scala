56b83df69516ae998086c54f53cbf3326fcac69b
CodeLionX <sebastian.schmidl@t-online.de>
2019-12-04 21:02:48 +0100

    disable kamon instrumentation for now

--- src/main/scala/com/github/codelionx/distod/Main.scala
+++ src/main/scala/com/github/codelionx/distod/Main.scala
@@ -2,11 +2,14 @@ package com.github.codelionx.distod
 
 import com.github.codelionx.distod.actors.{FollowerGuardian, LeaderGuardian}
 import com.typesafe.config.Config
+import kamon.Kamon
 
 
 object Main {
 
   def main(args: Array[String]): Unit = {
+//    Kamon.init()
+
     if (systemIsLeader(ActorSystem.defaultConfiguration)) {
       ActorSystem.create(LeaderGuardian())
     } else {

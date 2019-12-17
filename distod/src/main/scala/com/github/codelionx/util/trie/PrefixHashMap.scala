1589c9c05e58bd0cea1e1654d8b0cb4aa1ac11e8
CodeLionX <sebastian.schmidl@t-online.de>
2019-12-17 21:51:54 +0100

    prefix implementation version 2

--- distod/src/main/scala/com/github/codelionx/util/trie/PrefixHashMap.scala
+++ distod/src/main/scala/com/github/codelionx/util/trie/PrefixHashMap.scala
@@ -1,27 +1,27 @@
 package com.github.codelionx.util.trie
 
 import scala.annotation.tailrec
-import scala.collection.{mutable, Factory, StrictOptimizedMapOps}
+import scala.collection.{mutable, Factory, StrictOptimizedIterableOps}
 import scala.collection.immutable.SortedSet
 import scala.language.implicitConversions
 
 
-class PrefixHashMap[A, V]
-  extends mutable.Map[SortedSet[A], V]
-    with mutable.MapOps[SortedSet[A], V, mutable.Map, PrefixHashMap[A, V]]
-    with StrictOptimizedMapOps[SortedSet[A], V, PrefixHashMap, PrefixHashMap[A, V]] {
+class PrefixHashMap[A, K <: SortedSet[A], V]
+  extends mutable.Map[K, V]
+    with mutable.MapOps[K, V, mutable.Map, PrefixHashMap[A, K, V]]
+    with StrictOptimizedIterableOps[(K, V), mutable.Iterable, PrefixHashMap[A, K, V]] {
 
-  private val suffixes: mutable.Map[A, PrefixHashMap[A, V]] = mutable.Map.empty
+  private val suffixes: mutable.Map[A, PrefixHashMap[A, K, V]] = mutable.Map.empty
   private var value: Option[V] = None
 
-  override def get(key: SortedSet[A]): Option[V] =
+  override def get(key: K): Option[V] =
     if (key.isEmpty) value
     else
     // key.head is safe here
-      suffixes.get(key.head).flatMap(_.get(key.tail))
+      suffixes.get(key.head).flatMap(_.get(key.tail.asInstanceOf[K]))
 
   @tailrec
-  final def withPrefix(prefix: SortedSet[A]): PrefixHashMap[A, V] =
+  final def withPrefix(prefix: K): PrefixHashMap[A, K, V] =
     if (prefix.isEmpty) this
     else {
       // prefix.head is safe here
@@ -31,42 +31,40 @@ class PrefixHashMap[A, V]
           suffixes.update(head, empty)
         case _ =>
       }
-      suffixes(head).withPrefix(prefix.tail)
+      suffixes(head).withPrefix(prefix.tail.asInstanceOf[K])
     }
 
-  override def addOne(elem: (SortedSet[A], V)): PrefixHashMap.this.type = {
+  override def addOne(elem: (K, V)): PrefixHashMap.this.type = {
     withPrefix(elem._1).value = Some(elem._2)
     this
   }
 
-  override def subtractOne(key: SortedSet[A]): PrefixHashMap.this.type = {
+  override def subtractOne(key: K): PrefixHashMap.this.type = {
     if (key.isEmpty) {
       value = None
     } else {
       // key.head is safe here
-      suffixes.get(key.head).flatMap(_.remove(key.tail))
+      suffixes.get(key.head).flatMap(_.remove(key.tail.asInstanceOf[K]))
     }
     this
   }
 
-  override def iterator: Iterator[(SortedSet[A], V)] = {
-//      implicit @implicitNotFound("No implicit value for Ordering[${A}] found to build iterator") ev: Ordering[A]
-    implicit val orderingA: Ordering[A] = implicitly[Ordering[A]]
+  override def iterator: Iterator[(K, V)] = {
     val thisNode = for (
       v <- value.iterator
-    ) yield (SortedSet.empty[A], v)
+    ) yield (Iterator.empty[K], v)
     val suffixesIter = for {
       (suffix, otherMap) <- suffixes.iterator
       (suffixPart, v) <- otherMap
-    } yield (suffixPart.incl(suffix), v)
-    thisNode ++ suffixesIter
+    } yield (suffixPart incl suffix, v)
+    (thisNode ++ suffixesIter).asInstanceOf[Iterator[(K, V)]]
   }
 
   // Overloading of transformation methods that should return a PrefixMap
-  def map[W](f: ((SortedSet[A], V)) => (SortedSet[A], W)): PrefixHashMap[A, W] =
+  def map[W](f: ((K, V)) => (K, W)): PrefixHashMap[A, K, W] =
     strictOptimizedMap(PrefixHashMap.newBuilder, f)
 
-  def flatMap[W](f: ((SortedSet[A], V)) => IterableOnce[(SortedSet[A], W)]): PrefixHashMap[A, W] =
+  def flatMap[W](f: ((K, V)) => IterableOnce[(K, W)]): PrefixHashMap[A, K, W] =
     strictOptimizedFlatMap(PrefixHashMap.newBuilder, f)
 
   // Override `concat` and `empty` methods to refine their return type
@@ -75,16 +73,16 @@ class PrefixHashMap[A, V]
 //override def concat[W >: V](suffix: IterableOnce[(SortedSet[A], W)]): PrefixHashMap[A, W] =
 //  strictOptimizedConcat(suffix, PrefixHashMap.newBuilder)
 
-  override def empty: PrefixHashMap[A, V] = new PrefixHashMap
+  override def empty: PrefixHashMap[A, K, V] = new PrefixHashMap
 
   // Members declared in scala.collection.mutable.Clearable
   override def clear(): Unit = suffixes.clear()
 
   // Members declared in scala.collection.IterableOps
-  override protected def fromSpecific(coll: IterableOnce[(SortedSet[A], V)]): PrefixHashMap[A, V] =
-    PrefixHashMap.from(coll)
+  override protected def fromSpecific(coll: IterableOnce[(K, V)]): PrefixHashMap[A, K, V] =
+    PrefixHashMap.from[A, K, V](coll)
 
-  override protected def newSpecificBuilder: mutable.Builder[(SortedSet[A], V), PrefixHashMap[A, V]] =
+  override protected def newSpecificBuilder: mutable.Builder[(K, V), PrefixHashMap[A, K, V]] =
     PrefixHashMap.newBuilder
 
   override def className = "PrefixHashMap"
@@ -92,20 +90,20 @@ class PrefixHashMap[A, V]
 
 object PrefixHashMap {
 
-  def empty[A, V]: PrefixHashMap[A, V] = new PrefixHashMap[A, V]
+  def empty[A, K <: SortedSet[A], V]: PrefixHashMap[A, K, V] = new PrefixHashMap[A, K, V]
 
-  def from[A, V](it: IterableOnce[(SortedSet[A], V)]): PrefixHashMap[A, V] = it match {
-    case alreadyPrefixMap: PrefixHashMap[A, V] => alreadyPrefixMap
-    case _ => (newBuilder ++= it).result()
+  def from[A, K <: SortedSet[A], V](it: IterableOnce[(K, V)]): PrefixHashMap[A, K, V] = it match {
+    case alreadyPrefixMap: PrefixHashMap[A, K, V] @unchecked => alreadyPrefixMap
+    case _ => (newBuilder[A, K, V] ++= it).result()
   }
 
-  def newBuilder[A, V]: mutable.Builder[(SortedSet[A], V), PrefixHashMap[A, V]] =
-    new mutable.GrowableBuilder[(SortedSet[A], V), PrefixHashMap[A, V]](empty)
+  def newBuilder[A, K <: SortedSet[A], V]: mutable.Builder[(K, V), PrefixHashMap[A, K, V]] =
+    new mutable.GrowableBuilder[(K, V), PrefixHashMap[A, K, V]](empty)
 
-  implicit def toFactory[A, V](self: this.type): Factory[(SortedSet[A], V), PrefixHashMap[A, V]] =
-    new Factory[(SortedSet[A], V), PrefixHashMap[A, V]] {
-      override def fromSpecific(it: IterableOnce[(SortedSet[A], V)]): PrefixHashMap[A, V] = self.from(it)
+  implicit def toFactory[A , K <: SortedSet[A], V](self: this.type): Factory[(K, V), PrefixHashMap[A, K, V]] =
+    new Factory[(K, V), PrefixHashMap[A, K, V]] {
+      override def fromSpecific(it: IterableOnce[(K, V)]): PrefixHashMap[A, K, V] = self.from(it)
 
-      override def newBuilder: mutable.Builder[(SortedSet[A], V), PrefixHashMap[A, V]] = self.newBuilder
+      override def newBuilder: mutable.Builder[(K, V), PrefixHashMap[A, K, V]] = self.newBuilder
     }
 }

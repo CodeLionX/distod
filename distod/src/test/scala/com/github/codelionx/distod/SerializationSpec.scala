f61d08314bc9f4e608c436f42388666f68e2f9cb
CodeLionX <sebastian.schmidl@t-online.de>
2019-12-18 11:14:11 +0100

    fix serialization test and propberly shutdown system after tests to prevent resource leak

--- distod/src/test/scala/com/github/codelionx/distod/SerializationSpec.scala
+++ distod/src/test/scala/com/github/codelionx/distod/SerializationSpec.scala
@@ -3,28 +3,43 @@ package com.github.codelionx.distod
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
 import akka.serialization.SerializationExtension
-import com.github.codelionx.distod.types.CandidateSet
 import com.github.codelionx.distod.Serialization.CborSerializable
-import org.scalatest.{Matchers, WordSpec}
+import com.github.codelionx.distod.types.CandidateSet
 import org.scalatest.TryValues._
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
 import scala.util.Success
 
-
 object SerializationSpec {
+
   final case class Message(id: CandidateSet) extends CborSerializable
+
 }
 
-class SerializationSpec extends WordSpec with Matchers {
+class SerializationSpec extends WordSpec with Matchers with BeforeAndAfterAll {
 
   import SerializationSpec._
 
+  var system: akka.actor.typed.ActorSystem[Nothing] = _
+
+  override protected def beforeAll(): Unit = {
+    system = akka.actor.typed.ActorSystem(Behaviors.empty, "serialization-test-system")
+    super.beforeAll()
+  }
+
+  override protected def afterAll(): Unit = {
+    system.terminate()
+    Await.ready(system.whenTerminated, 2 seconds)
+    super.afterAll()
+  }
 
   "The serialization system" should {
-    val system = akka.actor.typed.ActorSystem(Behaviors.empty, "serialization-test-system")
-    val serialization = SerializationExtension(system.toClassic)
 
     "serialize messages with CandidateSets" in {
+      val serialization = SerializationExtension(system.toClassic)
       val testSubjects = Seq(
         CandidateSet.empty,
         CandidateSet.from(0, 5, 8, 2, 4),

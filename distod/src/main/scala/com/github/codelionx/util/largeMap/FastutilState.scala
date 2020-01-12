b67234bd1c02580cab73014a4e06e54cb427438a
CodeLionX <sebastian.schmidl@t-online.de>
2020-01-12 14:54:53 +0100

    make fastutil state inherit the mutable interfaces

--- distod/src/main/scala/com/github/codelionx/util/largeMap/FastutilState.scala
+++ distod/src/main/scala/com/github/codelionx/util/largeMap/FastutilState.scala
@@ -5,60 +5,56 @@ import com.github.codelionx.util.Math
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 
 import scala.collection.{mutable, StrictOptimizedIterableOps}
-import scala.collection.immutable.{AbstractMap, Iterable}
 import scala.jdk.CollectionConverters._
 
 
 object FastutilState {
+
+  val DEFAULT_NUMBER_OF_ATTRIBUTES: Int = 5
+
+  def empty[V]: FastutilState[V] = empty[V](DEFAULT_NUMBER_OF_ATTRIBUTES)
 
   def empty[V](nAttributes: Int): FastutilState[V] = new FastutilState(nAttributes, IndexedSeq.empty)
 
   def apply[V](nAttributes: Int, elems: (CandidateSet, V)*): FastutilState[V] = fromSpecific(nAttributes, elems)
 
   def fromSpecific[V](nAttributes: Int, coll: IterableOnce[(CandidateSet, V)]): FastutilState[V] = {
-    val b = newSpecificBuilder[V](nAttributes)
-    b.addAll(coll)
-    b.result()
+    val s = empty[V](nAttributes)
+    s.addAll(coll)
+    s
   }
 
-  def newSpecificBuilder[V](nAttributes: Int): mutable.Builder[(CandidateSet, V), FastutilState[V]] =
-    new StateBuilder[V](nAttributes)
-
-  private[FastutilState] class StateBuilder[V] private[FastutilState](
-      numberOfAttributes: Int,
-      startState: IndexedSeq[Object2ObjectOpenHashMap[CandidateSet, V]] = IndexedSeq.empty
-  ) extends mutable.Builder[(CandidateSet, V), FastutilState[V]] {
-
-    private var internalState: IndexedSeq[Object2ObjectOpenHashMap[CandidateSet, V]] = startState
-
-    override def addOne(elem: (CandidateSet, V)): this.type = {
-      val (key, value) = elem
-      // fill seq up with empty maps (prevents IndexOutOfBoundsException)
-      while (internalState.size <= key.size) {
-        val expectedSize = Math.binomialCoefficient(numberOfAttributes, internalState.size)
-        internalState :+= new Object2ObjectOpenHashMap(expectedSize, .9f)
-      }
-      // select correct map and update it with new mapping
-      val map = internalState(key.size)
-      map.put(key, value)
-      this
-    }
-
-    override def clear(): Unit = {
-      internalState = IndexedSeq.empty
-    }
-
-    override def result(): FastutilState[V] = new FastutilState(
-      numberOfAttributes,
-      internalState
-    )
-  }
+  def newBuilder[V](nAttributes: Int): mutable.Builder[(CandidateSet, V), FastutilState[V]] =
+    new mutable.GrowableBuilder[(CandidateSet, V), FastutilState[V]](empty[V](nAttributes))
 }
 
 
-class FastutilState[V] private(nAttributes: Int, levels: IndexedSeq[Object2ObjectOpenHashMap[CandidateSet, V]])
-  extends AbstractMap[CandidateSet, V]
-    with StrictOptimizedIterableOps[(CandidateSet, V), Iterable, FastutilState[V]] {
+class FastutilState[V] private(
+    private var nAttributes: Int,
+    private var levels: IndexedSeq[Object2ObjectOpenHashMap[CandidateSet, V]]
+)
+  extends mutable.AbstractMap[CandidateSet, V]
+    with StrictOptimizedIterableOps[(CandidateSet, V), mutable.Iterable, FastutilState[V]] {
+
+  /**
+   * Adjusts the array sizes of the internal maps to offer the best performance considering the expected number of
+   * elements per level. The expected number of elements is calculated from the level index and the supplied `size`.
+   * This method rebuilds all internal data structures (full copy) and therefore has a considerably performance hit.
+   * Use it sparsely.
+   *
+   * @param size number of attributes which form the cnadidate space
+   */
+  def reshapeMaps(size: Int): FastutilState.this.type = {
+    nAttributes = size
+    val reshapedMaps = levels.zipWithIndex.map { case (hashMap, levelIndex) =>
+      val expectedSize = Math.binomialCoefficient(nAttributes, levelIndex)
+      val updatedMap = new Object2ObjectOpenHashMap[CandidateSet, V](expectedSize, .9f)
+      updatedMap.putAll(hashMap)
+      updatedMap
+    }
+    levels = reshapedMaps
+    this
+  }
 
   private def factory: FastutilState.type = FastutilState
 
@@ -66,40 +62,9 @@ class FastutilState[V] private(nAttributes: Int, levels: IndexedSeq[Object2Objec
     factory.fromSpecific(nAttributes, coll)
 
   override def newSpecificBuilder: mutable.Builder[(CandidateSet, V), FastutilState[V]] =
-    factory.newSpecificBuilder(nAttributes)
+    factory.newBuilder(nAttributes)
 
   override def empty: FastutilState[V] = factory.empty(nAttributes)
-
-//  @inline override def -(key: CandidateSet): FastutilState[V] = removed(key)
-
-  override def removed(key: CandidateSet): FastutilState[V] = {
-    val selectedMap = levels(key.size)
-//    val updatedMap = selectedMap.clone()
-    selectedMap.remove(key)
-//    new FastutilState(nAttributes, levels.updated(key.size, updatedMap))
-    this
-  }
-
-  @inline override def +[V1 >: V](kv: (CandidateSet, V1)): FastutilState[V1] = updated(kv._1, kv._2)
-
-  override def updated[V1 >: V](key: CandidateSet, value: V1): FastutilState[V1] = {
-//    val levelIndex = key.size
-//    val levelClone = levels.zipWithIndex.map{
-//      case (m, `levelIndex`) => new Object2ObjectOpenHashMap[CandidateSet, V1](m, .9f)
-//      case (m, _) => m.asInstanceOf[Object2ObjectOpenHashMap[CandidateSet, V1]]
-//    }
-//    val b = new FastutilState.StateBuilder[V1](nAttributes, cloneLevels[V1])
-//    b.addOne(key -> value)
-//    b.result()
-    var l = levels
-    while (l.size <= key.size) {
-      val expectedSize = Math.binomialCoefficient(nAttributes, l.size)
-      l :+= new Object2ObjectOpenHashMap(expectedSize, .9f)
-    }
-    val l2 = l.asInstanceOf[IndexedSeq[Object2ObjectOpenHashMap[CandidateSet, V1]]]
-    l2(key.size).put(key, value)
-    new FastutilState[V1](nAttributes, l2)
-  }
 
   override def apply(key: CandidateSet): V =
     if (key.size >= levels.size)
@@ -120,39 +85,56 @@ class FastutilState[V] private(nAttributes: Int, levels: IndexedSeq[Object2Objec
       }
     }
 
-  override def iterator: Iterator[(CandidateSet, V)] = levels.iterator.flatMap(_.entrySet().asScala.map(e =>
-    e.getKey -> e.getValue
-  ))
+  override def subtractOne(key: CandidateSet): FastutilState.this.type = {
+    if (key.size < levels.size) {
+      val selectedMap = levels(key.size)
+      selectedMap.remove(key)
+    }
+    this
+  }
 
-//  override def updatedWith[V1 >: V](key: CandidateSet)(remappingFunction: Option[V] => Option[V1]): FastutilState[V1] = {
+  override def addOne(elem: (CandidateSet, V)): FastutilState.this.type = {
+    val (key, value) = elem
+    while (levels.size <= key.size) {
+      val expectedSize = Math.binomialCoefficient(nAttributes, levels.size)
+      levels :+= new Object2ObjectOpenHashMap(expectedSize, .9f)
+    }
+    levels(key.size).put(key, value)
+    this
+  }
+
+  override def iterator: Iterator[(CandidateSet, V)] = levels.iterator
+    .flatMap(
+      _.entrySet()
+        .asScala
+        .map(entry => entry.getKey -> entry.getValue)
+    )
+
+////  override def updatedWith[V1 >: V](key: CandidateSet)(remappingFunction: Option[V] => Option[V1]): FastutilState[V1] = {
+////    val previousValue = this.get(key)
+////    val nextValue = remappingFunction(previousValue)
+////    (previousValue, nextValue) match {
+////      case (None, None) => this.asInstanceOf[FastutilState[V1]]
+////      case (Some(_), None) => this.removed(key).asInstanceOf[FastutilState[V1]]
+////      case (_, Some(v)) => this.updated(key, v)
+////    }
+////  }
+//
+//  // we only call updatedWith with the same value type
+//  def updatedWith(key: CandidateSet)(remappingFunction: Option[V] => Option[V]): FastutilState[V] = {
 //    val previousValue = this.get(key)
 //    val nextValue = remappingFunction(previousValue)
 //    (previousValue, nextValue) match {
-//      case (None, None) => this.asInstanceOf[FastutilState[V1]]
-//      case (Some(_), None) => this.removed(key).asInstanceOf[FastutilState[V1]]
+//      case (None, None) => this
+//      case (Some(_), None) => this.removed(key)
 //      case (_, Some(v)) => this.updated(key, v)
 //    }
 //  }
-
-  // we only call updatedWith with the same value type
-  def updatedWith(key: CandidateSet)(remappingFunction: Option[V] => Option[V]): FastutilState[V] = {
-    val previousValue = this.get(key)
-    val nextValue = remappingFunction(previousValue)
-    (previousValue, nextValue) match {
-      case (None, None) => this
-      case (Some(_), None) => this.removed(key)
-      case (_, Some(v)) => this.updated(key, v)
-    }
-  }
-
-  // overrides that make return types more specific
-  override def concat[V1 >: V](suffix: collection.IterableOnce[(CandidateSet, V1)]): FastutilState[V1] = {
-    val b = new FastutilState.StateBuilder[V1](nAttributes, cloneLevels[V1])
-    b.addAll(suffix)
-    b.result()
-  }
-
-  private def cloneLevels[V1 >: V]: IndexedSeq[Object2ObjectOpenHashMap[CandidateSet, V1]] =
-//    levels.map(orig => new Object2ObjectOpenHashMap[CandidateSet, V1](orig, .9f))
-    levels.map(_.asInstanceOf[Object2ObjectOpenHashMap[CandidateSet, V1]])
+//
+//  // overrides that make return types more specific
+//  override def concat[V1 >: V](suffix: collection.IterableOnce[(CandidateSet, V1)]): FastutilState[V1] = {
+//    val b = new FastutilState.StateBuilder[V1](nAttributes, cloneLevels[V1])
+//    b.addAll(suffix)
+//    b.result()
+//  }
 }

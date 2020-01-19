71f56b4b7cb0d76de3a6d90d216c0b401fb9d444
CodeLionX <sebastian.schmidl@t-online.de>
2020-01-19 21:35:02 +0100

    improve time measuring script and add a parse script

--- deployment-single/measure.sh
+++ deployment-single/measure.sh
@@ -3,14 +3,14 @@
 jar=distod.jar
 config=bench.conf
 
-files=( "flight_1k_5c" "flight_1k_10c" "flight_1k_15c" "flight_1k_20c" "flight_1k_25c" "flight_1k_30c" )
-#header=( "no" "no" ) #"no" "yes" "yes" )
+declare -a files=( data/*.csv )
+#header=( "no" "no" "no" "yes" "yes" )
 
 for (( i=0; i<${#files[@]}; ++i )); do
   file=${files[i]}
   #hasHeader=${header[i]}
 
-  filename="bench-timing-${file}.txt"
+  filename="bench-timing-$(basename "${file}" ".csv").txt"
   echo "Benchmarking dataset ${file}"
   echo "Benchmarking dataset ${file}" >"${filename}"
   {
@@ -25,7 +25,7 @@ for (( i=0; i<${#files[@]}; ++i )); do
         taskset --cpu-list 1 java -Xmx8g -Xms8g \
           -Dconfig.file="${config}" \
           -Dlogback.configurationFile=logback.xml \
-          -Ddistod.input.path="data/${file}.csv" \
+          -Ddistod.input.path="${file}" \
           -Ddistod.input.has-header="yes" \
           -jar "${jar}" | grep "TIME"
       )

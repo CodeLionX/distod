1a5ca1ad2e228bc2ac7f99c6940840ceae5ad869
CodeLionX <sebastian.schmidl@t-online.de>
2020-01-21 15:15:53 +0100

    enable remote profiling using JMX

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# use VisualVm to connect to the process remotely via JMX: $hostname:9010
 java -Xms10g -Xmx10g \
+  -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false \
+  -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.rmi.port=9010 -Djava.rmi.server.hostname="${hostname}" \
   -Dconfig.file="$(hostname).conf" \
   -Dlogback.configurationFile=logback.xml \
   -jar distod.jar

c633c1fd978b632ce426b4af6c4f2df677c5e372
CodeLionX <sebastian.schmidl@t-online.de>
2020-02-11 10:15:43 +0100

    refactor timing implementation, so that it can be turned off statically

--- distod/src/main/scala/com/github/codelionx/util/timing/TimingSpans.scala
+++ distod/src/main/scala/com/github/codelionx/util/timing/TimingSpans.scala
@@ -3,22 +3,51 @@ package com.github.codelionx.util.timing
 import akka.actor.typed.ActorRef
 
 
-class TimingSpans private[timing](private var begins: Map[String, Long], ref: ActorRef[Command]) {
+trait TimingSpans {
 
   /**
    * Alias to `start`
+   *
    * @see [[com.github.codelionx.util.timing.TimingSpans#start]]
    */
   def begin(label: String): Unit = start(label)
 
-  def start(label: String): Unit = {
-    val start = System.nanoTime()
-    begins += label -> start
+  /**
+   * Alias to `stop`
+   *
+   * @see [[com.github.codelionx.util.timing.TimingSpans#stop]]
+   */
+  def end(label: String): Unit = stop(label)
+
+  def start(label: String): Unit
+
+  def stop(label: String): Unit
+}
+
+object TimingSpans {
+
+  private[timing] def apply(begins: Map[String, Long], ref: ActorRef[Command]): TimingSpans =
+    new TimingSpansImpl(begins, ref)
+
+  class TimingSpansImpl private[TimingSpans](private var begins: Map[String, Long], ref: ActorRef[Command])
+    extends TimingSpans {
+
+    def start(label: String): Unit = {
+      val start = System.nanoTime()
+      begins += label -> start
+    }
+
+    def stop(label: String): Unit = {
+      val end = System.nanoTime()
+      val start = begins.getOrElse(label, end)
+      ref ! AddTiming(label, start, end)
+    }
   }
 
-  def end(label: String): Unit = {
-    val end = System.nanoTime()
-    val start = begins.getOrElse(label, end)
-    ref ! AddTiming(label, start, end)
+  private[timing] object NoOpTimingSpans extends TimingSpans {
+    override def start(label: String): Unit = ()
+
+    override def stop(label: String): Unit = ()
   }
+
 }

b72de51788b00e88b606bc4142014e3806227b93
CodeLionX <sebastian.schmidl@t-online.de>
2020-08-27 17:47:00 +0200

    improve actor system implementation to make it more robust

--- distod/src/main/scala/com/github/codelionx/distod/ActorSystem.scala
+++ distod/src/main/scala/com/github/codelionx/distod/ActorSystem.scala
@@ -26,24 +26,21 @@ object ActorSystem {
    */
   case object FOLLOWER extends Role
 
-  val distodVersion = Version.current
-  val distodGitSHA = Version.gitSHA
+  val distodVersion: String = Version.current
+  val distodGitSHA: String = Version.gitSHA
+  private val versionInfoMsg = s"DISTOD version: $distodVersion\tDISTOD commit: $distodGitSHA"
 
-  def defaultConfiguration: Config = ConfigFactory.load()
+  val defaultConfiguration: Config = ConfigFactory.load()
 
   def create[T](userGuardian: Behavior[T]): akka.actor.typed.ActorSystem[T] = {
     val actorSystemName = defaultConfiguration.getString("distod.system-name")
+    create(actorSystemName, defaultConfiguration, userGuardian)
+  }
 
-    val versionInfoMsg = s"DISTOD version: $distodVersion\nDISTOD commit: $distodGitSHA"
-
-    val system = create(actorSystemName, defaultConfiguration, userGuardian)
+  def create[T](actorSystemName: String, config: Config, userGuardian: Behavior[T]): akka.actor.typed.ActorSystem[T] = {
+    val system = akka.actor.typed.ActorSystem(userGuardian, actorSystemName, config)
     println(versionInfoMsg)
     system.log.info(versionInfoMsg)
     system
   }
-
-  def create[T](actorSystemName: String, config: Config, userGuardian: Behavior[T]): akka.actor.typed.ActorSystem[T] =
-    akka.actor.typed.ActorSystem(userGuardian, actorSystemName, config)
-
-
 }

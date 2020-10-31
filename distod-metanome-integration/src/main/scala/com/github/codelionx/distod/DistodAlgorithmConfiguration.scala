c96abb15f7bd945eee71b4791b55c3e9f88f42c0
CodeLionX <sebastian.schmidl@t-online.de>
2020-10-31 23:34:54 +0100

    refactor code of DistodAlgorithmConfiguration to make it more consice

--- distod-metanome-integration/src/main/scala/com/github/codelionx/distod/DistodAlgorithmConfiguration.scala
+++ distod-metanome-integration/src/main/scala/com/github/codelionx/distod/DistodAlgorithmConfiguration.scala
@@ -2,13 +2,10 @@ package com.github.codelionx.distod
 
 import java.{lang, util}
 
-import com.github.codelionx.distod.DistodAlgorithm.inputGeneratorIdentifier
 import com.typesafe.config.{Config, ConfigFactory}
 import de.metanome.algorithm_integration.AlgorithmConfigurationException
 import de.metanome.algorithm_integration.algorithm_types.{BooleanParameterAlgorithm, IntegerParameterAlgorithm, StringParameterAlgorithm}
-import de.metanome.algorithm_integration.configuration.{ConfigurationRequirement, ConfigurationRequirementInteger, ConfigurationRequirementRelationalInput}
-
-import scala.jdk.CollectionConverters._
+import de.metanome.algorithm_integration.configuration.ConfigurationRequirement
 
 
 trait DistodAlgorithmConfiguration
@@ -23,49 +20,32 @@ trait DistodAlgorithmConfiguration
 
   protected def parseConfig: Config = ConfigFactory.parseMap(settings)
 
-  override def setStringConfigurationValue(identifier: String, values: String*): Unit = {
-    if (identifier != null)
-      if (DistodParameters.all.exists(_.identifier == identifier))
-        settings.put(identifier, values(0))
-      else
-        handleUnknownConfigurationValue(identifier, values)
-  }
-
-  override def setIntegerConfigurationValue(identifier: String, values: Integer*): Unit = {
-    if (identifier != null)
-      if (DistodParameters.all.exists(_.identifier == identifier)) {
-        if (!(identifier == DistodParameters.PruningOdSizeLimit.identifier &&
-          values(0) == DistodParameters.PruningOdSizeLimit.defaultValue) &&
-          !(identifier == DistodParameters.PruningInterestingnessThreshold.identifier &&
-            values(0) == DistodParameters.PruningInterestingnessThreshold.defaultValue))
-          settings.put(identifier, values(0))
-      } else
-        handleUnknownConfigurationValue(identifier, values)
-  }
-
-  override def setBooleanConfigurationValue(identifier: String, values: lang.Boolean*): Unit = {
-    if (identifier != null)
-      if (DistodParameters.all.exists(_.identifier == identifier))
-        settings.put(identifier, values(0))
-      else
-        handleUnknownConfigurationValue(identifier, values)
-  }
-
-  override def getConfigurationRequirements: util.ArrayList[ConfigurationRequirement[_]] = {
-    val configurationRequirement: ConfigurationRequirementInteger = {
-      val req = new ConfigurationRequirementInteger("test")
-      req.setDefaultValues(Array[java.lang.Integer](0))
-      req.setRequired(true)
-      req
+  override def setStringConfigurationValue(identifier: String, values: String*): Unit =
+    checked(identifier, values) {
+      settings.put(identifier, values(0))
     }
-    val reqs = Seq(
-      new ConfigurationRequirementRelationalInput(inputGeneratorIdentifier)
-    ) :+ configurationRequirement
-    new util.ArrayList(reqs.asJava)
-  }
 
-  protected def handleUnknownConfigurationValue(identifier: String, values: Seq[Any]): Unit =
-    throw new AlgorithmConfigurationException(
-      s"Unknown configuration for parameter $identifier! Supplied values: ${values.mkString(", ")}"
-    )
+  override def setIntegerConfigurationValue(identifier: String, values: Integer*): Unit =
+    checked(identifier, values) {
+      if (!(identifier == DistodParameters.PruningOdSizeLimit.identifier &&
+        values(0) == DistodParameters.PruningOdSizeLimit.defaultValue) &&
+        !(identifier == DistodParameters.PruningInterestingnessThreshold.identifier &&
+          values(0) == DistodParameters.PruningInterestingnessThreshold.defaultValue))
+        settings.put(identifier, values(0))
+    }
+
+  override def setBooleanConfigurationValue(identifier: String, values: lang.Boolean*): Unit =
+    checked(identifier, values) {
+      settings.put(identifier, values(0))
+    }
+
+  private def checked(identifier: String, values: Seq[Any])(f: => Unit): Unit =
+    if (identifier != null)
+      if (DistodParameters.all.exists(_.identifier == identifier))
+        f
+      else
+        throw new AlgorithmConfigurationException(
+          s"Unknown configuration for parameter $identifier! Supplied values: ${values.mkString(", ")}"
+        )
+
 }
